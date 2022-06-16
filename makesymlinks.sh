82a42cc4c046de3f1cbdeaec5489feff865f26da
Ludovico Funari <ludovico.funari@gmail.com>
2022-06-16 15:14:25 +0200

    Add unattended flag (useful for automated install)

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,7 +7,7 @@
 ########## Install vim, tmux, zsh, oh-my-zsh (with plugins), powerlevel10k
 
 sudo apt install vim tmux zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
